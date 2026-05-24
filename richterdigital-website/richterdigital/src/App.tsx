8da4331e632d0b03c5d3ac4053d54b78e8affdcd
arti2504 <arthur.richter90@gmail.com>
2026-05-24 17:37:14 +0200

    Initial deploy

---
+++ richterdigital-website/richterdigital/src/App.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import PrivacyPage from './pages/PrivacyPage';
+import ImpressumPage from './pages/ImpressumPage';
+
+function App() {
+  return (
+    <div className="relative min-h-screen bg-navy-900">
+      <div className="noise-overlay" />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/privacy" element={<PrivacyPage />} />
+        <Route path="/impressum" element={<ImpressumPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
